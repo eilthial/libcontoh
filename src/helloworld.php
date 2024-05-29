9e726667683d210042ed9af82ba539021ad0bc81
baim4424 <q5737089@gmail.com>
2024-05-29 09:30:49 +0700

    first commit

---
+++ src/helloworld.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace baim\libhallo;
+
+class helloworld{
+    public function sayHello($name){
+        return "Hello $name welcome to php and composer to create your on library";
+    
+    }
+}
